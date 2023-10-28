7ac826b057790a7ae1c90902bd2b09941f47235f
RGAlejandro <rubiogemesalejandro@gmail.com>
2023-10-28 13:08:47 +0200

    formulario contacto desde sesion activa

--- Formulario AGR/paginaUsuario.php
+++ Formulario AGR/paginaUsuario.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <title>Página con Inicio de Sesión y Registro</title>
     <style>
         body {
@@ -94,7 +95,7 @@
         <a href="#">Inicio</a>
         <a href="#">Acerca de</a>
         <a href="#">Servicios</a>
-        <a href="#">Contacto</a>
+        <a href="#" onclick="toggleContactPopup()">Contacto</a>
     </div>
     <?php
     // Inicia la sesión al principio del script
@@ -148,14 +149,26 @@
         modal.style.display = "none";
     }
     </script>              
-    <!-- <script>
-        function redirigirARegistro() {
-            window.location.href = "registro.php";
-        }
-    </script> -->
-                <!-- ¿No tienes una cuenta?<button onclick="redirigirARegistro()">Registrar</button> -->
+   
             </div>
         </div>
+    </div>
+    <div class="contact-popup-container" id="contactPopupContainer" style="display: none;">
+    <div class="contact-popup container" id="contactPopup">
+        <h2 class="mb-3">Contacto</h2>
+        <form id="contactForm" action="contacto.php" method="POST">
+            <div class="form-group">
+                <label for="contactEmail">Correo Electrónico</label>
+                <input type="text" class="form-control" id="contactEmail" name="contactEmail" placeholder="Correo Electrónico" value="<?php echo $_SESSION['email']; ?>" readonly>
+            </div>
+            <div class="form-group">
+                <label for="incidencia">Descripción de la incidencia</label>
+                <textarea class="form-control" id="incidencia" name="incidencia" placeholder="Descripción de la incidencia" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+    </div>
     </div>
     <script>
         function toggleLoginPopup() {
@@ -169,5 +182,29 @@
             }
         }
     </script>
+    <script>
+    function toggleContactPopup() {
+        var contactPopup = document.getElementById("contactPopup");
+        var contactPopupContainer = document.getElementById("contactPopupContainer");
+        
+        var loginPopup = document.getElementById("loginPopup");
+        var loginPopupContainer = document.getElementById("loginPopupContainer");
+        
+        // Oculta el formulario de inicio de sesión si está abierto
+        if (loginPopup.style.display === "block") {
+            loginPopup.style.display = "none";
+            loginPopupContainer.style.display = "none";
+        }
+        
+        // Muestra/oculta el formulario de contacto
+        if (contactPopup.style.display === "block") {
+            contactPopup.style.display = "none";
+            contactPopupContainer.style.display = "none";
+        } else {
+            contactPopup.style.display = "block";
+            contactPopupContainer.style.display = "block";
+        }
+    }
+</script>
 </body>
 </html>
